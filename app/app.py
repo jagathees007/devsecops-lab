fbc1bec9b37d0cf2958062002a88a470559347fc
jagathees007 <44640816+jagathees007@users.noreply.github.com>
2026-01-29 10:31:29 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,30 +1,7 @@
-import os
-import sqlite3
-from flask import Flask, request
+# Program to add two numbers
 
-app = Flask(__name__)
+a = 5
+b = 3
 
-@app.route("/user")
-def get_user():
-    name = request.args.get("name")
-
-    conn = sqlite3.connect("users.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE name = '" + name + "'")
-
-    return str(cur.fetchall())
-
-@app.route("/run")
-def run_cmd():
-    cmd = request.args.get("cmd")
-
-    os.system(cmd)
-
-    return "Command executed"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-def secure_function(user_input):
-    print(user_input)
-
+sum = a + b
+print("Sum:", sum)
